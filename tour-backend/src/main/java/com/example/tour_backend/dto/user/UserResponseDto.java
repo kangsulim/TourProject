ff3bfdc0a2408b19731d9f4884576f221be2b5c2
kijunsung <keejunsd@aver.com>
2025-07-07 17:39:21 +0900

    admin

--- tour-backend/src/main/java/com/example/tour_backend/dto/user/UserResponseDto.java
+++ tour-backend/src/main/java/com/example/tour_backend/dto/user/UserResponseDto.java
@@ -1,14 +1,18 @@
 package com.example.tour_backend.dto.user;
 
-
+import com.example.tour_backend.domain.Role;
 import com.example.tour_backend.domain.user.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
     private Long userId;
     private String username;
@@ -17,23 +21,42 @@ public class UserResponseDto {
     private String password;
     private String phone;
     private String nickname;
+    private Role role;
     private LocalDateTime createDate;
     private LocalDateTime modifiedDate;
 
-    public UserResponseDto() {}
+    /**
+     * 엔티티 → DTO 변환용 생성자
+     * (필드가 늘어나거나 바뀌면 이쪽만 고쳐 주시면 됩니다)
+     */
+    public UserResponseDto(User u) {
+        this.userId       = u.getUserId();
+        this.username     = u.getUsername();
+        this.name         = u.getName();
+        this.email        = u.getEmail();
+        this.password     = u.getPassword();
+        this.phone        = u.getPhone();
+        this.nickname     = u.getNickname();
+        this.role         = u.getRole();
+        this.createDate   = u.getCreateDate();
+        this.modifiedDate = u.getModifiedDate();
+    }
 
-    // User 엔티티를 받아 필드에 값 설정하는 생성자
-    public UserResponseDto(User user) {
-        this.userId = user.getUserId();
-        this.username = user.getUsername();
-        this.name = user.getName();
-        this.email = user.getEmail();
-        this.password = user.getPassword();
-        this.phone = user.getPhone();
-        this.nickname = user.getNickname();
-        this.createDate = user.getCreateDate();
-        this.modifiedDate = user.getModifiedDate();
-
-
+    /**
+     * Stream 매핑용 static 팩토리 메서드
+     */
+    public static UserResponseDto fromEntity(User u) {
+        return new UserResponseDto(
+                u.getUserId(),
+                u.getUsername(),
+                u.getName(),
+                u.getEmail(),
+                u.getPassword(),
+                u.getPhone(),
+                u.getNickname(),
+                u.getRole(),
+                u.getCreateDate(),
+                u.getModifiedDate()
+        );
     }
 }
