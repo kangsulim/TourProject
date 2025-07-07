9ad35946165bc75b2e5df87dd3a1e1c4a8fe8152
ojieunyango <ojieunyango@gmail.com>
2025-07-07 11:25:01 +0900

    add

--- tour-frontend/src/services/notificationApi.ts
+++ tour-frontend/src/services/notificationApi.ts
@@ -9,7 +9,12 @@ export const fetchNotifications = async (userId: number): Promise<Notification[]
   return res.data;
 };
 
-// 알림 삭제 -> 읽음처리로 수정 (db에 기록이 남게끔하기)
+// 알림 삭제 -> 읽음처리로 수정 (db에 기록이 남게끔하기) 이거 안쓰고 있음
 export const deleteNotifications = async (ids: number[]) => {
   return api.delete(`/notifications`, { data: { ids } });
+};
+
+//  읽음 처리 API 함수 
+export const markAsRead = async (noticeId: number): Promise<void> => {
+  await api.patch(`/notifications/${noticeId}/read`);
 };
